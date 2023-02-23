19ac07d7dcef14b943a7a42af7409004241b1628
Florin Georgescu <flo_georgescu@hotmail.com>
2023-02-23 15:21:23 +0200

    fix: Fixing local setup with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,25 +26,25 @@ const WalletAuthTypeOrmModule = () => {
   let typeOrmOptions: TypeOrmModuleOptions;
   const baseConnectionOptions: TypeOrmModuleOptions = process.env.DATABASE_URL
     ? {
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      entities,
-      synchronize: false,
-      autoLoadEntities: true,
-      logging: ['info'],
-    }
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities,
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: ['info'],
+      }
     : {
-      type: 'postgres',
-      host: process.env.DB_HOST ?? 'localhost',
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_DATABASE || 'wallet',
-      entities,
-      synchronize: false,
-      autoLoadEntities: true,
-      logging: ['info'],
-    };
+        type: 'postgres',
+        host: process.env.DB_HOST ?? 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        database: process.env.DB_DATABASE || 'wallet',
+        entities,
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: ['info'],
+      };
 
   if (!!process.env.DATABASE_CERT) {
     typeOrmOptions = {
@@ -54,12 +54,16 @@ const WalletAuthTypeOrmModule = () => {
         ca: process.env.DATABASE_CERT,
       },
     };
+  } else if (process.env.DB_SSL_REJECT_UNAUTHORIZED === 'false') {
+    typeOrmOptions = {
+      ...baseConnectionOptions,
+    };
   } else {
     typeOrmOptions = {
       ...baseConnectionOptions,
       ssl: {
         rejectUnauthorized: false,
-      }
+      },
     };
   }
   return TypeOrmModule.forRoot(typeOrmOptions);
@@ -77,4 +81,4 @@ const WalletAuthTypeOrmModule = () => {
     AuthModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
